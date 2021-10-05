5e047505ce644b39ca763758d4343ccdbdd1908a
Parham Alvani <1995parham@tuta.io>
2021-10-05 23:15:16 +0330

    fix: correct lint issues

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"os"
 	"time"
@@ -11,12 +10,13 @@ import (
 	"github.com/plgd-dev/go-coap/v2/udp"
 )
 
-// ArgsNum is the number of required arguments for application
+// ArgsNum is the number of required arguments for application.
 const ArgsNum = 3
 
 func main() {
 	if len(os.Args) != ArgsNum {
-		fmt.Printf("usage: coap-demo [address e.g. 192.168.73.192:1378] [path e.g. elahe]\n")
+		log.Printf("usage: coap-demo [address e.g. 192.168.73.192:1378] [path e.g. elahe]\n")
+
 		return
 	}
 
@@ -32,19 +32,25 @@ func main() {
 
 	resp, err := co.Get(ctx, path)
 	if err != nil {
-		log.Fatalf("error sending request: %v", err)
+		log.Printf("error sending request: %v", err)
+
+		return
 	}
 
 	log.Printf("response code: %s", resp.Code())
 
 	body, err := resp.ReadBody()
 	if err != nil {
-		log.Fatal("cannot read the payload %v", err)
+		log.Printf("cannot read the payload %v", err)
+
+		return
 	}
 
 	var r map[string]interface{}
 	if err := json.Unmarshal(body, &r); err != nil {
-		log.Fatal(err)
+		log.Printf("cannot parse the payload as json %v", err)
+
+		return
 	}
 
 	log.Printf("response payload: %+v", r)
